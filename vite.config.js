1a5103637ed4df57dd0a11ddd0f0b250a7d911b0
[Waseem] <[waseemkhan3703@gmail.com]>
2025-07-27 12:09:06 +0500

    Fixing errors

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path';
 
 
 // https://vite.dev/config/
@@ -10,3 +11,4 @@ export default defineConfig({
     outDir: 'dist',
   },
 })
+// vite.config.js
